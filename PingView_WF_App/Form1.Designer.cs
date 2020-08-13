78d2c4e993908bb6c295ec036ae70d48ffa89693
jiejasonliu <jasonningliu@gmail.com>
2020-08-13 14:43:20 -0600

    Initial commit with basic UI

---
+++ PingView_WF_App/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PingView_WF_App {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.ipTextbox = new System.Windows.Forms.TextBox();
+            this.pingButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.AutoSize = true;
+            this.ipLabel.Font = new System.Drawing.Font("Orbitron", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipLabel.Location = new System.Drawing.Point(54, 47);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(35, 20);
+            this.ipLabel.TabIndex = 0;
+            this.ipLabel.Text = "IP :";
+            // 
+            // ipTextbox
+            // 
+            this.ipTextbox.Font = new System.Drawing.Font("Orbitron", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipTextbox.Location = new System.Drawing.Point(95, 45);
+            this.ipTextbox.Name = "ipTextbox";
+            this.ipTextbox.Size = new System.Drawing.Size(324, 25);
+            this.ipTextbox.TabIndex = 1;
+            // 
+            // pingButton
+            // 
+            this.pingButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pingButton.Font = new System.Drawing.Font("Orbitron", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pingButton.Location = new System.Drawing.Point(325, 76);
+            this.pingButton.Name = "pingButton";
+            this.pingButton.Size = new System.Drawing.Size(94, 33);
+            this.pingButton.TabIndex = 2;
+            this.pingButton.Text = "Ping";
+            this.pingButton.UseVisualStyleBackColor = false;
+            this.pingButton.Click += new System.EventHandler(this.PingButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Orbitron", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(54, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Standby...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 257);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pingButton);
+            this.Controls.Add(this.ipTextbox);
+            this.Controls.Add(this.ipLabel);
+            this.Name = "Form1";
+            this.Text = "PingView (C#)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.TextBox ipTextbox;
+        private System.Windows.Forms.Button pingButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PingView_WF_App/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace PingView_WF_App {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void PingButton_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
